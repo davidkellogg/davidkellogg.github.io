e6ceba32cbf745063495b7963b52970fb63a2c42
David Kellogg <david.kellogg2@icloud.com>
2021-01-19 20:45:18 -0800

    Created Mothership class to control DOMbots

--- development/js/DOMbot.js
+++ development/js/DOMbot.js
@@ -1,54 +1,88 @@
 "use strict";
 
-export default function DOMbot( blueprints = undefined, secret = undefined ) {
-  // declare private variables
-  const _secret = secret;
-  const _id = `#${blueprints.id}`;
+export let _secret = Math.floor( ( Math.random() * 100000000 + 10000000 ) );
 
-  // declare public interface
-  Object.defineProperties( this, {
-    id: {
-      get: () => _id,
-      set: ( value ) => {
-        if ( value.secret === _secret ) {
-          Action( value.data );
-        } else {
-          console.info( "cannot set value of `id`" );
+class Mothership {
+  constructor( blueprints, secret ) {
+    const _secret = secret;
+    const _data = blueprints.data;
+
+    let variables = blueprints.variables;
+
+    for ( let i = blueprints.validations.length - 1; i >= 0; i-- ) {
+      Object.defineProperty( this, blueprints.validations[i].varIndex, {
+        get: () => variables[blueprints.validations[i].varIndex],
+        set: ( value ) => blueprints.validations[i].action( value )
+      } )
+    }
+  }
+}
+class DOMbot {
+  constructor( blueprints = undefined, secret = undefined ) {
+    // declare private variables
+    const _secret = secret;
+    const _id = `#${blueprints.id}`;
+
+    // declare public interface
+    Object.defineProperties( this, {
+      id: {
+        get: () => _id,
+        set: ( value ) => {
+          if ( value.secret === _secret ) {
+            Action( value.data );
+          } else {
+            console.info( "cannot set value of `id`" );
+          }
         }
       }
+    } );
+
+    // insert JS into DOM
+    function Insert() {
+      if ( blueprints.Insert !== undefined ) {
+        document.querySelector( blueprints.Insert.nearbyId ).insertAdjacentHTML( blueprints.Insert.position, blueprints.Insert.text );
+      } else undefined;
     }
-  } );
+    Insert();
 
-  // insert JS into DOM
-  function Insert() {
-    if ( blueprints.Insert !== undefined ) {
-      document.querySelector( blueprints.Insert.nearbyId ).insertAdjacentHTML( blueprints.Insert.position, blueprints.Insert.text );
-    } else undefined;
+    // attach event
+    function Event() {
+      if ( blueprints.Event !== undefined && typeof blueprints.Event.Listener == "function" ) {
+        document.querySelector( _id ).addEventListener( `${blueprints.Event.type}`, ( event ) => {
+          event.preventDefault();
+          blueprints.Event.Listener();
+        } );
+      } else undefined;
+    }
+    Event();
+
+    // declare Action
+    function Action() {
+      if ( typeof blueprints.Action === "function" ) {
+        Action = blueprints.Action;
+      } else undefined;
+    }
+    Action();
+
+    // this.SelfDestruct = () => document.querySelector("#search").remove()
+    // cannot delete bot, but can delete element bot is looking at + making bot = undefined
+
+    // lock bot from modification
+    Object.freeze(this);
   }
-  Insert();
+}
 
-  // attach event
-  function Event() {
-    if ( blueprints.Event !== undefined && typeof blueprints.Event.Listener == "function" ) {
-      document.querySelector( _id ).addEventListener( `${blueprints.Event.type}`, ( event ) => {
-        event.preventDefault();
-        blueprints.Event.Listener();
-      } );
-    } else undefined;
+// send command to DOMbots
+export let SendCommand = ( bot, data ) => {
+  bot.id = {
+    secret : _secret,
+      data : data
   }
-  Event();
+}
 
-  // declare Action
-  function Action() {
-    if ( typeof blueprints.Action === "function" ) {
-      Action = blueprints.Action;
-    } else undefined;
+export function Factory( blueprints ) {
+  window[blueprints.name] = new Mothership( blueprints, _secret );
+  for ( let i = 0, len = blueprints.bots.length; i < len; i++ ) {
+    window[`${blueprints.bots[i].id}_BOT`] = new DOMbot( blueprints.bots[i], _secret );
   }
-  Action();
-
-  // this.SelfDestruct = () => document.querySelector("#search").remove()
-  // cannot delete bot, but can delete element bot is looking at + making bot = undefined
-
-  // lock bot from modification
-  Object.freeze(this);
 }
--- development/js/search.js
+++ development/js/search.js
@@ -6,13 +6,56 @@
     firefox -> about:config -> privacy.file_unique_origin = false
 */
 
-import DOMbot from "./DOMbot.js"
+// import functions
+import { _secret, SendCommand, Factory } from "./DOMbot.js"
 
-// Search constructor
-function Search( secret = undefined ) {
-  // key for Search and DOMbot to talk to each other
-  const _secret = secret;
-  const _index  = (() => {
+// returns keys of the array based on keyword prevelance
+let Query = ( array, keywords ) => {
+  // declare private to `this.keywords`
+  let relScores = [];
+  let relToSort = [];
+
+  // iterate through _index
+  for ( let i = 0, len = array.length; i < len; i++ ) {
+    let n = 0;
+    let indexItem = array[i];
+    indexItem.name.toLowerCase();
+    indexItem.description.toLowerCase();
+
+    // iterate through _keywords
+    for ( let j = keywords.length - 1; j >= 0; j-- ) {
+      let k = 0;
+      while (true) {
+        k = indexItem.description.indexOf( keywords[j], k );
+        if (k >= 0) {
+          n++;
+          k++;
+        } else break;
+      }
+      n += indexItem.name.includes( keywords[j] ) ? 2 : 0;
+    } // end _keywords iterations
+
+    relScores.push( n );
+    relToSort.push( i );
+  } // end _index iterations
+
+  // return filtered and sorted array
+  return relToSort.filter( (a) => relScores[a] > 0 ).sort( ( b, c ) => relScores[c] - relScores[b] );
+}
+
+// returns the reordered `array` by `keys` or undefined if either array is empty
+let Reorder = ( array, keys ) => {
+  let sortedArray = [];
+  for ( let i = 0, len = keys.length; i < len; i++ ) {
+    sortedArray.push( array[ keys[i] ] );
+  }
+  return sortedArray;
+}
+
+// blueprints
+const blueprints = {
+  name : "search",
+  data : (() => {
     const li = document.querySelectorAll("li");
     let tmpIndex = [];
     for ( let i = 0, len = li.length; i < len; i++) {
@@ -25,237 +68,150 @@ function Search( secret = undefined ) {
       tmpIndex.push(data);
     }
     return tmpIndex;
-  })();
-
-  // define private variables
-  let _delta    = 10;
-  let _entries  =  0;
-  let _keywords = [];
-  let _start    =  0;
-
-  let _relSorted = [];
-
-  // returns keys of the array based on keyword prevelance
-  let Query = ( array, keywords ) => {
-    // declare private to `this.keywords`
-    let relScores = [];
-    let relToSort = [];
-
-    // iterate through _index
-    for ( let i = 0, len = array.length; i < len; i++ ) {
-      let n = 0;
-      let indexItem = array[i];
-      indexItem.name.toLowerCase();
-      indexItem.description.toLowerCase();
-
-      // iterate through _keywords
-      for ( let j = keywords.length - 1; j >= 0; j-- ) {
-        let k = 0;
-        while (true) {
-          k = indexItem.description.indexOf( keywords[j], k );
-          if (k >= 0) {
-            n++;
-            k++;
-          } else break;
-        }
-        n += indexItem.name.includes( keywords[j] ) ? 2 : 0;
-      } // end _keywords iterations
-
-      relScores.push( n );
-      relToSort.push( i );
-    } // end _index iterations
-
-    // return filtered and sorted array
-    return relToSort.filter( (a) => relScores[a] > 0 ).sort( ( b, c ) => relScores[c] - relScores[b] );
-  }
-
-  // returns the reordered `array` by `keys` or undefined if either array is empty
-  let Reorder = ( array, keys ) => {
-    let sortedArray = [];
-    for ( let i = 0, len = keys.length; i < len; i++ ) {
-      sortedArray.push( array[ keys[i] ] );
-    }
-    return sortedArray;
-  }
-
-  // send command to DOMbots
-  let SendCommand = ( bot, data ) => {
-    bot.id = {
-      secret : _secret,
-        data : data
-    }
-  }
-
-  // define public interface
-  Object.defineProperties( this, {
-    delta: {
-      get: () => _delta,
-      // if value is 0 or negative, assign 1
-      // if value exceeds the number of entries, assign original value
-      // otherwise, assign value
-      set: ( value ) => {
-        if ( _entries > 0 ) {
-          _delta = value < 1 ? 1 : ( value > _entries ? _entries : value );
-          SendCommand( results_BOT,  Reorder( _index, _relSorted ).slice( _start, _start + _delta ) );
+  })(),
+  variables : {
+    start : 0,
+    delta : 10,
+    entries : 0,
+    keywords : [],
+    sorted : []
+  },
+  validations : [
+    {
+      varIndex : "start",
+      action : ( value ) => {
+        blueprints.variables.start = value < 0 ? 0 : ( value > blueprints.variables.entries ? blueprints.variables.start : value );
+        SendCommand( results_BOT,  Reorder( blueprints.data, blueprints.variables.sorted ).slice( blueprints.variables.start, blueprints.variables.start + blueprints.variables.delta ) );
+        SendCommand( previous_BOT, undefined );
+        SendCommand( next_BOT,     undefined );
+      }
+    },
+    {
+      varIndex : "delta",
+      action : ( value ) => {
+        if ( blueprints.variables.entries > 0 ) {
+          blueprints.variables.delta = value < 1 ? 1 : ( value > blueprints.variables.entries ? blueprints.variables.entries : value );
+          SendCommand( results_BOT,  Reorder( _data, blueprints.variables.sorted ).slice( blueprints.variables.start, blueprints.variables.start + blueprints.variables.delta ) );
           SendCommand( previous_BOT, undefined );
           SendCommand( next_BOT,     undefined );
         }
       }
     },
-    entries: {
-      get: () => _entries,
-      set: ( undefined ) => console.info( "cannot set the value of `entries`" )
+    {
+      varIndex : "entries",
+      action : undefined
     },
-    keywords: {
-      get: () => _keywords,
-      set: ( value ) => {
+    {
+      varIndex : "keywords",
+      action : ( value ) => {
         // speed function
         let timestamp = Date.now();
-
         if ( value !== "" ) {
           // data validation for keyword input
-          _keywords =   value instanceof Array  ? value              // if array, return array
-                    : ( value instanceof String ? value.split(" ")   // if string, return array of substrings
-                    : [ "" + value ] );                              // otherwise, return array of stringified value
-
-          _relSorted = Query( _index, _keywords );
+          blueprints.variables.keywords = value.split(" ");
+          blueprints.variables.sorted = Query( blueprints.data, blueprints.variables.keywords );
         } else {
-          _relSorted.length = 0;
+          // reset to original ordering
+          blueprints.variables.sorted = Array.from( blueprints.data.keys() );
         }
-
         // update private variables
-        _entries = _relSorted.length;
-        _start   = 0;
-
+        blueprints.variables.entries = blueprints.variables.sorted.length;
+        blueprints.variables.start   = 0;
         SendCommand( speed_BOT,    Date.now() - timestamp );
-        SendCommand( results_BOT,  Reorder( _index, _relSorted ).slice( _start, _start + _delta ) );
-        SendCommand( previous_BOT, undefined );
-        SendCommand( next_BOT,     undefined );
-      }
-    },
-    start: {
-      get: () => _start,
-      // if value is negative, assign 0
-      // if value exceeds the number of entries, assign original value
-      // otherwise, assign value
-      set: ( value ) => {
-        _start = value < 0 ? 0 : ( value > _entries ? _start : value );
-        SendCommand( results_BOT,  Reorder( _index, _relSorted ).slice( _start, _start + _delta ) );
+        SendCommand( results_BOT,  Reorder( blueprints.data, blueprints.variables.sorted ).slice( blueprints.variables.start, blueprints.variables.start + blueprints.variables.delta ) );
         SendCommand( previous_BOT, undefined );
         SendCommand( next_BOT,     undefined );
       }
     }
-  } );
-
-  // go back a page
-  this.Decrement = () => this.start -= _delta; // references properties instead of private lets to utilize data validation
-
-  // go forward a page
-  this.Increment = () => this.start += _delta; // references properties instead of private lets to utilize data validation
-
-  // insulate object from defineProperty manipulations
-  Object.seal(this);
-}
-
-// blueprints for DOMbot
-const blueprints = [
-  { // #search
-    id: "search",
-    Insert: {
-      nearbyId: "#results",
-      position: "beforebegin",
-      text:
-       `<form id="search">
-          <link rel="stylesheet" href="../css/navigation.css">
-          <input type="text" placeholder="Search for keywords..."></input>
-          <input type="submit" value="Search"></input>
-        </form>`
+  ],
+  bots: [
+    { // #search
+      id: "search",
+      Insert: {
+        nearbyId: "#results",
+        position: "beforebegin",
+        text:
+         `<form id="search">
+            <link rel="stylesheet" href="../css/navigation.css">
+            <input type="text" placeholder="Search for keywords..."></input>
+            <input type="submit" value="Search"></input>
+          </form>`
+      },
+      Event: {
+        type: "submit",
+        Listener: () => search.keywords = document.querySelector( `#search > input[type=text]` ).value.toLowerCase()
+      },
+      Action: undefined
     },
-    Event: {
-      type: "submit",
-      Listener: () => search.keywords = document.querySelector( `#search > input[type=text]` ).value.toLowerCase()
+    { // #speed
+      id: "speed",
+      Insert: {
+        nearbyId: "#results",
+        position: "beforebegin",
+        text: `<p id="speed" style="visibility: hidden;">0 records found in 0 milliseconds</p>`
+      },
+      Event: undefined,
+      Action: ( data = undefined ) => {
+        document.querySelector( "#speed" ).setAttribute( "style", "" );
+        document.querySelector( "#speed" ).innerHTML = `${search.entries} records found in ${data >= 1 ? data : "<1"} millisecond${data > 1 ? "s" : ""}`;
+      }
     },
-    Action: undefined
-  },
-  { // #speed
-    id: "speed",
-    Insert: {
-      nearbyId: "#results",
-      position: "beforebegin",
-      text: `<p id="speed" style="visibility: hidden;">0 records found in 0 milliseconds</p>`
-    },
-    Event: undefined,
-    Action: ( data = undefined ) => {
-      document.querySelector( "#speed" ).setAttribute( "style", "" );
-      document.querySelector( "#speed" ).innerHTML = `${search.entries} records found in ${data >= 1 ? data : "<1"} millisecond${data > 1 ? "s" : ""}`;
-    }
-  },
-  { // #next
-    id: "next",
-    Insert: {
-      nearbyId: "#results",
-      position: "afterend",
-      text: `<a id="next" class="insertPagination" style="display: none;" href="javascript:void(0)">Next</a>`
-    },
-    Event: {
-      type: "click",
-      Listener: () => search.Increment()
-    },
-    Action: ( data = undefined ) => {
-      document.querySelector( "#next" ).setAttribute( "style", search.start + search.delta < search.entries ? "" : "visibility: hidden;" );
-    }
-  },
-  { // #previous
-    id: "previous",
-    Insert: {
-      nearbyId: "#results",
-      position: "afterend",
-      text: `<a id="previous" class="insertPagination" style="display: none;" href="javascript:void(0)">Prev</a>`
-    },
-    Event: {
-      type: "click",
-      Listener: () => search.Decrement()
-    },
-    Action: ( data = undefined ) => {
-      document.querySelector( "#previous" ).setAttribute( "style", search.start > 0 ? "" : "visibility: hidden;" );
-    }
-  },
-  { // #results
-    id: "results",
-    Insert: undefined,
-    Event: undefined,
-    Action: ( data = undefined ) => {
-      document.querySelector( "#results" ).innerHTML = "";
-      if ( data !== undefined ) {
-        for ( let i = 0, len = data.length; i < len; i++ ) {
-          document.querySelector( "#results" ).insertAdjacentHTML( 'beforeend',
-            `<li>
-              <a href="${data[i].url}">
-                <article>
-                  <h4>${data[i].name}</h4>
-                  <time datetime="${data[i].datetime}">${new Date( data[i].datetime ).toLocaleDateString( "en-US", { year: 'numeric', month: 'long', day: 'numeric' } )}</time>
-                  <hr>
-                  <p>${data[i].description}</p>
-                </article>
-              </a>
-            </li>`
-          );
+    { // #results
+      id: "results",
+      Insert: undefined,
+      Event: undefined,
+      Action: ( data = undefined ) => {
+        document.querySelector( "#results" ).innerHTML = "";
+        if ( data !== undefined ) {
+          for ( let i = 0, len = data.length; i < len; i++ ) {
+            document.querySelector( "#results" ).insertAdjacentHTML( 'beforeend',
+              `<li>
+                <a href="${data[i].url}">
+                  <article>
+                    <h4>${data[i].name}</h4>
+                    <time datetime="${data[i].datetime}">${new Date( data[i].datetime ).toLocaleDateString( "en-US", { year: 'numeric', month: 'long', day: 'numeric' } )}</time>
+                    <hr>
+                    <p>${data[i].description}</p>
+                  </article>
+                </a>
+              </li>`
+            );
+          }
         }
       }
+    },
+    { // #next
+      id: "next",
+      Insert: {
+        nearbyId: "#results",
+        position: "afterend",
+        text: `<a id="next" class="insertPagination" style="display: none;" href="javascript:void(0)">Next</a>`
+      },
+      Event: {
+        type: "click",
+        Listener: () => search.start += search.delta
+      },
+      Action: ( data = undefined ) => {
+        document.querySelector( "#next" ).setAttribute( "style", search.start + search.delta < search.entries ? "" : "visibility: hidden;" );
+      }
+    },
+    { // #previous
+      id: "previous",
+      Insert: {
+        nearbyId: "#results",
+        position: "afterend",
+        text: `<a id="previous" class="insertPagination" style="display: none;" href="javascript:void(0)">Prev</a>`
+      },
+      Event: {
+        type: "click",
+        Listener: () => search.start -= search.delta
+      },
+      Action: ( data = undefined ) => {
+        document.querySelector( "#previous" ).setAttribute( "style", search.start > 0 ? "" : "visibility: hidden;" );
+      }
     }
-  }
-];
+  ]
+};
 
-// instantiate Search and DOMbot
-( ( Con, blueprints ) => {
-    let secret = Math.floor( ( Math.random() * 100000000 + 10000000 ) );
-
-    for ( let i = 0, len = blueprints.length; i < len; i++ ) {
-      window[`${blueprints[i].id}_BOT`] = new DOMbot( blueprints[i], secret );
-    }
-
-    window[Con[0]] = new Con[1]( secret );
-
-    secret = undefined;
-  }
-)( [ "search", Search ], blueprints );
+// instantiate Mothership and DOMbots
+Factory( blueprints );
