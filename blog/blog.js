511811eda9cb4c90a06ae4015349c7979987a020
David Kellogg <david.kellogg2@icloud.com>
2021-03-24 07:14:14 -0700

    Redesigned blog search functionality around custom elements

--- blog/blog.js
+++ blog/blog.js
@@ -1,46 +1,90 @@
 "use strict";
 
 // import functions
-import sanitize from "../js/sanitize.js";
-import search from "../js/search.js";
 import reorder from "../js/reorder.js";
 
-// insert form stylesheet
-document.head.insertAdjacentElement( "beforeend", (() => {
-  const element = document.createElement( "link" );
-  element.setAttribute( "rel", "stylesheet" );
-  element.setAttribute( "href", "../css/search.css" );
-  return element;
-})());
+customElements.define( "search-bot", class extends HTMLElement {
+  constructor() {
+    const TIMESTAMP = Date.now();
+    super();
+    new Promise( ( resolve ) => {
+      // set slot for list
+      this.querySelector("li").parentElement.setAttribute( "slot", "list");
 
-// find list
-const list = document.querySelector("ol");
+      // create shadowRoot and pass through chain
+      resolve( this.attachShadow({ mode: "closed" }) );
 
-// create form
-const form = document.createElement( "form" )
-form.setAttribute( "id", "search" );
+    // insert form stylesheet
+    }).then( ( shadowRoot ) => {
+      shadowRoot.appendChild( (() => {
+        const ELEMENT = document.createElement( "link" );
+        ELEMENT.setAttribute( "rel", "stylesheet" );
+        ELEMENT.setAttribute( "href", "../css/search.css" );
+        return ELEMENT;
+      })());
+      return shadowRoot;
 
-// create text input
-form.appendChild( (() => {
-  const element = document.createElement( "input" );
-  element.setAttribute( "type", "text" );
-  element.setAttribute( "placeholder", "Search for keywords..." );
-  return element;
-})());
+    // create form
+    }).then( ( shadowRoot ) => {
+      const form = document.createElement( "form" );
 
-// create submit button
-form.appendChild( (() => {
-  const element = document.createElement( "input" );
-  element.setAttribute( "type", "submit" );
-  element.setAttribute( "value", "Search" );
-  return element;
-})());
+      // create text input
+      const input = form.appendChild( (() => {
+        const ELEMENT = document.createElement( "input" );
+        ELEMENT.setAttribute( "type", "text" );
+        ELEMENT.setAttribute( "placeholder", "Search for keywords..." );
+        return ELEMENT;
+      })());
 
-// add submit event listener
-form.onsubmit = ( event ) => {
-  event.preventDefault();
-  reorder( search( sanitize( event.target.children[0].value ), list ), list );
-};
+      // create submit button
+      form.appendChild( (() => {
+        const ELEMENT = document.createElement( "input" );
+        ELEMENT.setAttribute( "type", "submit" );
+        ELEMENT.setAttribute( "value", "Search" );
+        return ELEMENT;
+      })());
 
-// insert form
-list.insertAdjacentElement( "beforebegin", form );
+      // add submit event listener
+      form.onsubmit = ( event ) => {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setAttribute( "search", input.value );
+      };
+
+      // append form
+      shadowRoot.appendChild( form );
+      return shadowRoot;
+
+    // append slot for list
+    }).then( ( shadowRoot ) => {
+      shadowRoot.appendChild( (() => {
+        const ELEMENT = document.createElement( "slot" );
+        ELEMENT.setAttribute( "name", "list" );
+        return ELEMENT
+      })());
+
+    // close out promise
+    }).then( () => {
+      Object.freeze(this);
+      console.info( `'search-bot' initialized in ${Date.now() - TIMESTAMP}ms` );
+    }).catch( ( error ) => { console.error( error ) });
+  } // end of constructor
+
+  // set default attributes
+  connectedCallback(){
+    this.setAttribute( "search" , "" );
+    // this.setAttribute( "display" , "10" );
+  }
+
+  // watch for attribute changes
+  static get observedAttributes() { return [ 'search', 'display' ] };
+  attributeChangedCallback( name, oldValue, newValue ) {
+    if ( name === "search"
+      && newValue.length > 2 ) {
+        reorder( newValue, this.querySelector("*[slot='list']") );
+    }
+  }
+
+  // disconnectedCallback(){}
+  // adoptedCallback(){}
+});
--- js/reorder.js
+++ js/reorder.js
@@ -1,5 +1,35 @@
-export default function reorder( reorderBy, reorderIn ) {
-  Array.from( reorderBy, ( key ) => reorderIn.children[key] ).forEach( ( child ) => {
-    reorderIn.appendChild( child );
+"use strict";
+import sanitize from "./sanitize.js";
+
+export default function reorder( forString, inList ) {
+  const TIMESTAMP = Date.now();
+  const SEARCHSTRING = sanitize( forString );
+
+  const SEARCHARRAY = new Array( SEARCHSTRING.length - 2 ).fill( SEARCHSTRING ).map( ( searchItem, index ) => {
+    // following line adapted from
+    // https://stackoverflow.com/questions/3115150/how-to-escape-regular-expression-special-characters-using-javascript
+    // accessed March 12, 2021
+    return new RegExp( searchItem.substr( index, 3 ).replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&"), "g" )
   });
+
+  const RELEVANCE = new Array( inList.children.length ).fill( 0 );
+
+  Array.from( inList.children ).forEach( ( node, index ) => {
+    let text = ""
+    node.querySelectorAll( "*" ).forEach( ( subNode ) =>  {
+      text += subNode.innerText
+    });
+    SEARCHARRAY.forEach( ( searchItem ) => {
+      try {
+        // following line adapted from
+        // https://stackoverflow.com/questions/4009756/how-to-count-string-occurrence-in-string
+        // accessed March 12, 2021
+        RELEVANCE[index] += ( text.match( searchItem ) || [] ).length;
+      } finally {}
+    });
+  });
+
+  Array.from( Array.from( RELEVANCE.keys() ).sort( ( a, b ) => RELEVANCE[b] - RELEVANCE[a] ), ( key ) => inList.children[key] )
+    .forEach( ( child ) => { inList.appendChild( child ) });
+  console.info( `searched for ${SEARCHSTRING} in ${Date.now() - TIMESTAMP}ms` );
 };
